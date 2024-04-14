983db413b920065920772d8599ee8ed4b7cc847c
EngMohammed123 <mk2596292@gmail.com>
2024-04-15 00:21:42 +0200

    best seller list view done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:bookly/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -10,26 +10,36 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-  return  const Padding(
-    padding: EdgeInsets.symmetric(horizontal: 20),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-        SizedBox(height: 50,),
-        Padding(
-          padding: EdgeInsets.only(left: 8),
-          child: Text(
-            'Best Seller',
-            style: Styles.textStyle18,
-          ),
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+             CustomAppBar(),
+             FeaturedBooksListView(),
+             SizedBox(height: 50,),
+             Padding(
+               padding: EdgeInsets.only(left: 8),
+               child: Padding(
+                 padding: EdgeInsets.symmetric(horizontal: 30),
+                 child: Text(
+                   'Best Seller',
+                   style: Styles.textStyle18,
+                 ),
+               ),
+             ),
+             SizedBox(height: 20,),
+             ]
+         ),
         ),
-        SizedBox(height: 20,),
-        BestSellerListViewItem(),
-        
-      ],
-  ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
+          ),
+        )
+      ]
   );
-  }
+}
 }
--- lib/features/home/presentation/views/widgets/index.dart
+++ lib/features/home/presentation/views/widgets/index.dart
@@ -1,3 +1,4 @@
+export 'best_seller_list_view.dart';
 export 'best_seller_list_view_item.dart';
 export 'book_rating.dart';
 export 'custom_app_bar.dart';
